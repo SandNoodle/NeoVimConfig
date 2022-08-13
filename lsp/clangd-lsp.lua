724a47a945fc1e8015626f91dafa12212a4465bf
HelloImCDD <cddgamer@gmail.com>
2022-08-13 18:11:10 +0200

    Add clangd commandline commands

--- lsp/clangd-lsp.lua
+++ lsp/clangd-lsp.lua
@@ -3,5 +3,17 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 require'lspconfig'.clangd.setup {
-	capabilites = capabilites
+	capabilites = capabilites,
+	cmd = {
+		"clangd",
+		"-j=4",
+		"--pretty",
+		"--clang-tidy",
+		"--inlay-hints",
+		"--background-index",
+		"--pch-storage=memory",
+		"--all-scopes-completion",
+		"--header-insertion=never",
+		"--suggest-missing-includes",
+	}
 }
